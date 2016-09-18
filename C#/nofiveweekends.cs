67dad71cd545383b6cb2232035150894ae713a2c
EvanZodl <ezorigami@comcast.net>
2016-09-18 18:15:47 -0400

    More Efficient Algorithm

--- C#/nofiveweekends.cs
+++ C#/nofiveweekends.cs
@@ -1,71 +1,60 @@
-//Code written and compiled using the online compiler at http://rextester.com/
-
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 
-namespace Rextester
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+public class Test {
+    
+    //Method to determine which day of the week the next year starts on,
+    //given the first day of the current year and whether or not it is a leap year
+    public static int nextYearFirstDay(int currentYearFirstDay, bool isLeapYear){
+        if (isLeapYear){
+           return (366 + currentYearFirstDay) % 7;
+        }
+        
+        else{
+            return (365 + currentYearFirstDay) % 7;
+        } 
+    }
+    
+    //Method to determine if a year does not have a month with 5
+    //weekends. his only happens on leap years that start on a
+    //Sunday and normal years that start on a Monday
+    public static bool hasNoFiveWeekends(bool isLeapYear, int firstDayOfYear){
+        
+        if(firstDayOfYear == 0 && isLeapYear){
+            return true;   
+        }
+        else if(firstDayOfYear == 1 && !isLeapYear){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+        
+    //Declare the year range constants
+    public const int startYear = 2000;
+    public const int endYear = 2100;
+   
+    //Sunday is 0, Monday is 1, Tuesday is 2, etc.
+    public static int firstDayOfYear = 6; //Jan 1, 2000 was a Saturday
+    public static bool isLeapYear;
+    
+    public static void Main(string[] args){
+        
+        //Loop through the specified year range
+        for(int i = startYear; i <= endYear; i++){
             
-            bool isLeapYear;
-            int numDaysInYear = 365; 
-            int startYear = 2000;
-            int endYear = 2100;
-            int day = 1; //Start at 1st day of the year
-            int dayOfWeek = 6; // Jan 1, 2000 was a Saturday
-            
-            
-            //Increment the year
-            for(int i = startYear; i <= endYear; i++){
-                bool fiveWeekends = false;
-                
-                //Leap year (adjust number days)
-                if(i%4 == 0){
-                    numDaysInYear = 366;
-                    isLeapYear = true;
-                }
-                
-                //Non-leap year
-                else{
-                    numDaysInYear = 365; 
-                    isLeapYear = false;
-                }
-                
-                //Increment the day
-                for (int j = day; j < numDaysInYear + 1; j++){
-                    
-                    //Check if any 31-day month starts with Friday
-                    //Below are the numbers of the days that start 31 day months
-                    if((j == 1 || j == 61 || j == 122 || j == 183 || j == 214 ||  j == 275 || j == 336)
-                        && dayOfWeek == 5 && isLeapYear && !fiveWeekends){
-                        fiveWeekends = true;
-                    }
-                    
-                    if((j == 1 || j == 60 || j == 121 || j == 182 || j == 213 || j == 274 || j == 335)
-                        && dayOfWeek == 5 && !isLeapYear && !fiveWeekends){
-                        fiveWeekends = true;
-                    }
-                    
-                    //Restart the counter after Sunday
-                    if(dayOfWeek == 7){
-                        dayOfWeek = 1;
-                    }
-                    
-                    else{
-                        dayOfWeek++;
-                    }             
-                    
-                    //Print the results in a list
-                    if(j == 365 && !fiveWeekends){
-                        Console.Write(i + ", ");
-                    }
-                }  
+            //Check if it is a leap year
+            isLeapYear = false;
+            if(i%4 == 0){
+                isLeapYear = true;
             }
+            
+            //Print out the non-five weekend years
+            if(hasNoFiveWeekends(isLeapYear, firstDayOfYear)){
+                Console.Write(i + ", ");
+            }
+                
+            firstDayOfYear = nextYearFirstDay(firstDayOfYear, isLeapYear);
         }
     }
 }
